85a12ea95426bb6fa14bf21da0439c1a9b42804e
Federico Jaeggi <fritzjaeggi2109@gmail.com>
2023-09-19 14:37:35 -0300

    add recharts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,95 @@
 import Link from 'next/link';
 import MainLayout from '../components/layouts/MainLayout';
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from 'recharts';
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 export default function Home() {
   return (
-    <MainLayout title="Home" description="Home Page">
+    <MainLayout title='Home' description='Home Page'>
       <h1>Home Page</h1>
-      <h1 className="title">
+      <h1 className='title'>
         Go to <Link href='/about'>About</Link>
       </h1>
+      <div style={{width: '300px', height: '300px'}}>
+        <ResponsiveContainer width='100%' height='100%'>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type='monotone'
+              dataKey='pv'
+              stroke='#8884d8'
+              activeDot={{ r: 8 }}
+            />
+            <Line type='monotone' dataKey='uv' stroke='#82ca9d' />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </MainLayout>
   );
 }
